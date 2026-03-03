20c3312c08de3dfcbcfdc0de2096b04038142f62
Storm <ricardonorton18@gmail.com>
2026-03-03 23:38:44 +0000

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,1 +1,9 @@
+local Solar = loadstring(game:HttpGet("your-url-here"))()
 
+local Window = Solar:CreateWindow({
+    Name = "My Script",
+    Config = {}
+})
+
+local MainTab = Window:CreateTab("Main", "7733970523")
+local SettingsTab = Window:CreateTab("Settings", "7733954760")
